23047f178365c40effac14080e4a58c483336cb2
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-01 22:02:30 +0900

    :sparkles: feat: add renderer

--- primer-three/src/ts/main.ts
+++ primer-three/src/ts/main.ts
@@ -5,3 +5,10 @@ const scene = new THREE.Scene();
 
 // カメラを追加
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// レンダラーを追加
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
