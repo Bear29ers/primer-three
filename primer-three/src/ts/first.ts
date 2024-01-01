023cbfaac3e057bf604745d808355c483a347500
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-01 22:42:08 +0900

    :sparkles: feat: create geometry

--- primer-three/src/ts/first.ts
+++ primer-three/src/ts/first.ts
@@ -5,10 +5,14 @@ const scene = new THREE.Scene();
 
 // カメラを追加
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 500);
 
 // レンダラーを追加
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// ジオメトリを作成
+const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
 
 renderer.render(scene, camera);
