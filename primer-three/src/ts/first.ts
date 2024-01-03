3a70edb8c70463eee49b751665d72b38e4b88553
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-03 23:21:06 +0900

    :sparkles: feat: render after the page is loaded

--- primer-three/src/ts/first.ts
+++ primer-three/src/ts/first.ts
@@ -1,53 +1,65 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// シーンを追加
-const scene = new THREE.Scene();
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let renderer: THREE.WebGLRenderer;
+let pointLight: THREE.PointLight;
+let controls: OrbitControls;
 
-// カメラを追加
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-// カメラの位置を変更する
-camera.position.set(0, 0, 500);
+const init = () => {
+  // シーンを追加
+  scene = new THREE.Scene();
 
-// レンダラーを追加
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
+  // カメラを追加
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+  // カメラの位置を変更する
+  camera.position.set(0, 0, 500);
 
-// テクスチャを追加
-const texture = new THREE.TextureLoader().load('/textures/earth.jpg');
+  // レンダラーを追加
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
 
-// ジオメトリを作成
-const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+  // テクスチャを追加
+  const texture = new THREE.TextureLoader().load('/textures/earth.jpg');
 
-// マテリアルを作成
-const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
+  // ジオメトリを作成
+  const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
 
-// メッシュ化
-const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-scene.add(ballMesh);
+  // マテリアルを作成
+  const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
 
-// 平行光源を追加
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(1, 1, 1);
-scene.add(directionalLight);
+  // メッシュ化
+  const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+  scene.add(ballMesh);
 
-// ポイント光源を追加
-const pointLight = new THREE.PointLight(0xffffff, 1);
-pointLight.position.set(-200, -200, -200);
-// 光の減衰率を設定
-pointLight.decay = 1;
-// 光源強度の設定
-pointLight.power = 1000;
-scene.add(pointLight);
+  // 平行光源を追加
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
 
-// ポイント光源の場所を視覚化
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
-scene.add(pointLightHelper);
+  // ポイント光源を追加
+  pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(-200, -200, -200);
+  // 光の減衰率を設定
+  pointLight.decay = 1;
+  // 光源強度の設定
+  pointLight.power = 1000;
+  scene.add(pointLight);
 
-// マウス操作を設定
-const controls = new OrbitControls(camera, renderer.domElement);
+  // ポイント光源の場所を視覚化
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+  scene.add(pointLightHelper);
+
+  // マウス操作を設定
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // eslint-disable-next-line no-use-before-define
+  animate();
+};
 
 // ポイント光源を巡回
 const animate = () => {
@@ -59,9 +71,11 @@ const animate = () => {
 
   requestAnimationFrame(animate);
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   controls.update();
 
   // レンダリング
   renderer.render(scene, camera);
 };
-animate();
+
+window.addEventListener('load', init);
