4f808a6950b3f08118039642508c4bae9a9d1693
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-03 20:48:58 +0900

    :sparkles: feat: add directional light

--- primer-three/src/ts/first.ts
+++ primer-three/src/ts/first.ts
@@ -5,6 +5,7 @@ const scene = new THREE.Scene();
 
 // カメラを追加
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+// カメラの位置を変更する
 camera.position.set(0, 0, 500);
 
 // レンダラーを追加
@@ -18,9 +19,14 @@ const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
 // マテリアルを作成
 const ballMaterial = new THREE.MeshPhysicalMaterial();
 
-// メッシュ化する
+// メッシュ化
 const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-// メッシュをシーンに追加する
 scene.add(ballMesh);
 
+// 平行光源を追加
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight);
+
+// レンダリング
 renderer.render(scene, camera);
