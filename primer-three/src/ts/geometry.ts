f01db8ff9ca817c6a3b41749007e90bbcc4d169b
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-04 22:09:47 +0900

    :sparkles: feat: create box geometry

--- primer-three/src/ts/geometry.ts
+++ primer-three/src/ts/geometry.ts
@@ -14,8 +14,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 document.body.appendChild(renderer.domElement);
 
+// ジオメトリを作成
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+// const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16);
+
 // マテリアル
-const metrial = new THREE.MeshNormalMaterial();
+const material = new THREE.MeshNormalMaterial({
+  // wireframe: true,
+});
+
+// メッシュ化
+const box = new THREE.Mesh(boxGeometry, material);
+scene.add(box);
 
 // ライト
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
