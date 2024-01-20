4e77cfba29fa470c88a395c7ad9877653fbde9d7
Yuta Okuma <y.dream.nest@icloud.com>
2024-01-20 20:22:28 +0900

    :construction: wip: prepare for creating materials

--- primer-three/src/ts/materialMesh.ts
+++ primer-three/src/ts/materialMesh.ts
@@ -1,65 +1,15 @@
-import GUI from 'lil-gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// UIデバッグ
-const gui = new GUI();
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let renderer: THREE.WebGLRenderer;
 
-// シーン
-const scene = new THREE.Scene();
-
-// カメラ
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(1, 1, 2);
-
-// レンダラー
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-document.body.appendChild(renderer.domElement);
-
-// ジオメトリ
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-
-// マテリアル
-const material = new THREE.MeshBasicMaterial({
-  color: 'red',
-});
-
-// メッシュ化
-const box = new THREE.Mesh(boxGeometry, material);
-scene.add(box);
-
-// デバッグ
-// フォルダ
-const positionFolder = gui.addFolder('position');
-const visibleFolder = gui.addFolder('visible');
-
-// -3~3の間を0.01ずつ移動できる
-// gui.add(box.position, 'x', -3, 3, 0.01);
-positionFolder.add(box.position, 'x').min(-3).max(3).step(0.01).name('transformX');
-positionFolder.add(box.position, 'y').min(-3).max(3).step(0.01).name('transformY');
-positionFolder.add(box.position, 'z').min(-3).max(3).step(0.01).name('transformZ');
-
-gui.add(box.rotation, 'x').min(-3).max(3).step(0.01).name('rotationX');
-
-// 表示・非表示
-visibleFolder.add(box, 'visible');
-// ワイヤーフレームON・OFF
-visibleFolder.add(material, 'wireframe');
-
-// カラーを変更
-gui.addColor(material, 'color');
-
-// ライト
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-scene.add(ambientLight);
-
-// マウス操作
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const controls = new OrbitControls(camera, renderer.domElement);
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-controls.enableDamping = true;
+const animate = () => {
+  // レンダリング
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+};
 
 // ブラウザのリサイズに対応
 const onWindowResize = () => {
@@ -68,19 +18,27 @@ const onWindowResize = () => {
   camera.updateProjectionMatrix();
 };
 
-window.addEventListener('resize', onWindowResize);
+const init = () => {
+  // シーン
+  scene = new THREE.Scene();
 
-const clock = new THREE.Clock();
+  // カメラ
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+  camera.position.set(1, 1, 2);
 
-const animate = () => {
-  const elapsedTime = clock.getElapsedTime();
+  // レンダラー
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  document.body.appendChild(renderer.domElement);
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-  controls.update();
+  // マウス操作
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-  // レンダリング
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+  window.addEventListener('resize', onWindowResize);
+
+  animate();
 };
 
-animate();
+window.addEventListener('load', init);
